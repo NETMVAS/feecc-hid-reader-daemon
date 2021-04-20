bf206d57dbeef040430678d6a2cbb2dd67497e1d
arseniiarsenii <arseniivelichko2@gmail.com>
2021-04-20 20:43:33 +0300

    reworked the daemon for the task

--- async_executor.py
+++ async_executor.py
@@ -2,14 +2,27 @@ import asyncio
 import requests
 from EventToInternet.KeyboardListener import KeyboardListener
 
-
-class BarcodeKeyboardListener(KeyboardListener):
-    async def dict_handler(self, dict_event):
-        print(dict_event)
-        requests.post("https://webhook.site/595ddd9f-de34-4af8-845c-c52bb2614083", json=dict_event)
+API_ENDPOINT = "http://127.0.0.1:8080/api/rfid"
 
 
-BarcodeKeyboardListener()
+class RfidKeyboardListener(KeyboardListener):
+    """listen to the R20D-USB-8H10D RFID reader in the background"""
 
-loop = asyncio.get_event_loop()
-loop.run_forever()
+    async def dict_handler(self, dict_event: dict) -> None:
+        """handle RFID scan event and post data to a Rest API endpoint"""
+
+        requests.post(
+            url=API_ENDPOINT,
+            json=dict_event
+        )
+
+
+# start the daemon
+if __name__ == "__main__":
+
+    # initialize an instance of the class
+    RfidKeyboardListener()
+
+    # run an endless async process
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
