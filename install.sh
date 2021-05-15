c4b3cc788e8ddad3a4e548d7f27deba4180ed4d8
arseniiarsenii <arseniivelichko2@gmail.com>
2021-05-15 13:45:45 +0300

    removed redundant auto-update daemon that was causing unnecessary IO load

--- install.sh
+++ install.sh
@@ -3,16 +3,9 @@
 sudo systemctl daemon-reload
 sudo systemctl stop EventToInternet.service
 sudo systemctl disable EventToInternet.service
-sudo systemctl stop EventToInternetUpdate.service
-sudo systemctl disable EventToInternetUpdate.service
 sudo git pull --force
 sudo cp -f /etc/systemd/system/EventToInternet/EventToInternet.service /etc/systemd/system/EventToInternet.service
-sudo cp -f /etc/systemd/system/EventToInternet/EventToInternetUpdate.service /etc/systemd/system/EventToInternetUpdate.service
 sudo systemctl daemon-reload
 sudo systemctl enable EventToInternet.service
 sudo systemctl start EventToInternet.service
-sudo systemctl enable EventToInternetUpdate.service
-sudo systemctl start EventToInternetUpdate.service
-
 sudo systemctl status EventToInternet.service
-sudo systemctl status EventToInternetUpdate.service
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,4 @@
 sudo systemctl daemon-reload
 sudo systemctl stop EventToInternet.service
 sudo systemctl disable EventToInternet.service
-sudo systemctl stop EventToInternetUpdate.service
-sudo systemctl disable EventToInternetUpdate.service
 sudo systemctl daemon-reload
